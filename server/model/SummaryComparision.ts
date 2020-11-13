dbc75d4671abafe956edaa74afe3be23af6f7aff
Srinath Gaddipati <srinath@Srinaths-MacBook-Pro-2.local>
2020-11-14 01:24:24 +0530

    Added SummaryComparision 1

---
+++ server/model/SummaryComparision.ts
@@ -0,0 +1,22 @@
+import ComparisonFactory from "./ComparisonFactory"
+import IComparison from "./IComparison";
+import SelectedFiles from "./SelectedFiles";
+
+class SummaryComparison {
+
+    comparedFiles : SelectedFiles
+    comparisions : Array<IComparison>
+    factory: ComparisonFactory
+
+    constructor(comparedFiles : SelectedFiles, comparisions : Array<IComparison>, factory: ComparisonFactory) {
+        this.comparedFiles = comparedFiles;
+        this.comparisions = comparisions;
+        this.factory = factory
+    }
+
+    function generateComparisions() : void {
+
+    }
+}
+
+export default SummaryComparision
